c589ebe5dd01045ad8113bab4e0c6e1eed2045e6
Antoni Grzymala <antoni@Poodle.localdomain>
2020-08-20 16:38:14 +0200

    Fixed an error that was left over from my private experiments

--- protools-to-pyramix.sh
+++ protools-to-pyramix.sh
@@ -10,12 +10,11 @@ NAME="$5"
 NO_REALLY="$5"
 
 for take in $(seq -w 01 $2); do
-
   tracknoraw=1
   cat "${MAPFILE}" | while read pttrackname; do
     trackno=$(printf "%02d" ${tracknoraw})
     src="${pttrackname}_${take}.wav"
-    dst=$(echo "${src}" | sed -r 's/^.*-mon_(..)\.wav$/'${NAME}'_'0${take}'_##'${trackno}'##_.wav/')
+    dst=$(echo "${src}" | sed -r 's/^.*_(..)\.wav$/'${NAME}'_'0${take}'_##'${trackno}'##_.wav/')
     [[ $5 == "yes" ]] \
       &&      mv -iv "${SRCDIR}/${src}" "${DSTDIR}/${dst}" \
       || echo mv -iv "${SRCDIR}/${src}" "${DSTDIR}/${dst}"
